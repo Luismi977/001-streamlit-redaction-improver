7883f553a6a082522cb829d4bcb03f4d027153f8
Luismi977 <Lmargolles77@gmail.com>
2025-07-07 07:52:52 +0200

    probando_2

--- main.py
+++ main.py
@@ -51,8 +51,8 @@ def load_LLM(openai_api_key):
 
 
 #Page title and header
-st.set_page_config(page_title="Re-write your text")
-st.header("Re-write your text")
+st.set_page_config(page_title="Re-escribe tu texto")
+st.header("Re-escribe tu texto")
 
 
 #Intro: instructions
